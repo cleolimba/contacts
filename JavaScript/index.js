88e8a6884686004b3c515358918eab9745a40965
concilie limba <limbavangu@gmail.com>
2024-05-10 00:27:14 +0100

    une partie

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,14 @@
-Complete Responsive Admin Dashboard - Code[Free Download] | HTML, CSS and JavaScript
+// ajout d'une classe survolée pour sélectionner un élément de liste
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink(){
+    list.forEach(item=> {
+        item.classList.remove("hovered");
+    })
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouscover", activeLink))
 
 
 // function bergerMenu() {
