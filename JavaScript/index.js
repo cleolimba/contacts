b17a1230e3c80f0f3203f4978b14b77b35f92930
Concilie Limba <limbavangu@gmail.com>
2024-05-09 12:30:59 +0100

    fais le premier bouton

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,8 @@
-function bergerMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-  }
+Complete Responsive Admin Dashboard - Code[Free Download] | HTML, CSS and JavaScript
+
+
+// function bergerMenu() {
+//     var menu = document.querySelector('.menu');
+//     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+//   }
   
