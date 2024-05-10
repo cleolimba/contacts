59f6df501a4272a320f8e77fb11ed2e568f7b791
Concilie Limba <limbavangu@gmail.com>
2024-05-10 12:17:26 +0100

    je suis bloquue

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,16 +1,27 @@
 // ajout d'une classe survolée pour sélectionner un élément de liste
-let list = document.querySelectorAll(".navigation li");
+// let list = document.querySelectorAll(".navigation li");
 
-function activeLink(){
-    list.forEach(item=> {
-        item.classList.remove("hovered");
-    })
-    this.classList.add("hovered");
-}
+// function activeLink(){
+//     list.forEach(item=> {
+//         item.classList.remove("hovered");
+//     })
+//     this.classList.add("hovered");
+// }
 
-list.forEach(item => item.addEventListener("mouscover", activeLink))
+// list.forEach(item => item.addEventListener("mouscover", activeLink))
 
+// menue
 
+// function toggleMenu() {
+//     const navMobile = document.querySelector('.navMobile');
+//     const partieOverlay = document.querySelector('.navMobile');
+
+//     if(navMobile.classList.contains('active')) {
+//         navMobile.classList.add('active');
+//     } else {
+//         navMobile.classList.add('active');
+//     }
+// }
 // function bergerMenu() {
 //     var menu = document.querySelector('.menu');
 //     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
