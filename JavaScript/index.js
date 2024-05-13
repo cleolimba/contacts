b4ae19098f149f0ecbac6486a03c52a542fe15e1
concilie limba <limbavangu@gmail.com>
2024-05-13 08:17:15 +0100

    ajout des fonctions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,42 @@
+const valDome = document.getElementById('btn');
+const deleteMenu = document.getElementById('main-menu');
+
+
+
+valDome.addEventListener('click', function(){
+        const changeWidthContent = document.getElementById('main-content');
+        if(changeWidthContent.classList.contains("main-content-fill")){
+            changeWidthContent.classList.remove("main-content-fill");
+            deleteMenu.classList.remove("hide-menu");
+        }
+        else{
+            changeWidthContent.classList.add("main-content-fill");
+            deleteMenu.classList.add("hide-menu");
+        }
+} 
+
+)
+
+
+
+// const domValu = document.getElementById("btn");
+// console.log(domValu)
+// domValu.addEventListener("click",function(){
+//     if (domValu.style.display === "none") {
+//         document.getElementById("main-menu").style.display = "block";
+//         document.getElementById("main").style.display = "block";
+//         // document.getElementById("main-content").style.marginLeft ="25%";
+//         // document.getElementById("main-content").style.width ="80%";
+//     }
+//     else{
+//         document.getElementById("main-menu").style.display = "none";
+//         document.getElementById("main-content").style.marginLeft ="0%";
+//         document.getElementById("main-content").style.width ="100%";
+//     }
+// });
+
+
+
 // ajout d'une classe survolée pour sélectionner un élément de liste
 // let list = document.querySelectorAll(".navigation li");
 
