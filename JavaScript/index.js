d9a032fefee7b53add12a6bc9cb1064c7bf9dc34
Concilie Limba <limbavangu@gmail.com>
2024-05-14 12:29:05 +0100

    les fonctionnalites javascrit de plus

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -35,3 +35,44 @@ document.querySelector('.addContact').addEventListener('click', function(){
         popupForm.classList.add("invisible");
     }
 });
+
+// propriéte pour enregistre le tableau
+
+function contact_table() {
+    let body = document.getElementsByTagName("body") [0];
+    let tbl = document.createElement("table");
+    let tblBody = document.createElement("tbody");
+
+    // for (var i = 0; i < 2; i++) {
+        let row = document.createElement("tr");
+        // for (var j = 0; j < 2; j++) {
+            let cell = document.createElement("td");
+            // let text = "valeur "+ j;
+            // cell.textContent=text;
+            row.appendChild(cell);
+        }
+        // tblBody.appendChild(row);
+    
+    // tbl.appendChild(tblBody);
+    // body.appendChild(tbl);
+    // tbl.setAttribute("border", "2");
+
+
+// bouton enregistre
+
+document.querySelector('.btnContact').addEventListener('click', function(){
+    
+    const main_content = document.getElementById('main-content');
+    const contactsContent = document.getElementById('contents');
+    const tableauContacts = document.getElementById('tableau');
+
+    if (tableauContacts.classList.contains("invisibles")) {
+    console.log(document.getElementById('contents'))
+        contactsContent.classList.add("invisibles");
+        tableauContacts.classList.remove("invisibles");
+        
+    }else {
+        contactsContent.classList.remove("invisibles");
+        tableauContacts.classList.add("invisibles");
+    }
+});
